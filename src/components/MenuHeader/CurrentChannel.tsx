408ca44365eae213e32a049318e1b1f4b10f0802
psoni01 <cmungcharoenpo00@mylangara.ca>
2022-10-12 01:07:04 -0700

    organize: MenuHeader.tsx

---
+++ src/components/MenuHeader/CurrentChannel.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ChannelJSON } from '../../types/channel'
+import { StyledSpan, StyledColLogoContainer } from './StyledComponents'
+
+type Props = {
+  channel: ChannelJSON
+  selectChannel: any
+}
+
+const CurrentChannel: React.FC<Props> = ({ channel, selectChannel }) => {
+  return (
+    <StyledColLogoContainer span={4}>
+      {selectChannel && (
+        <Link to="/select-channel">
+          <StyledSpan color="#ffffff">
+            {channel?.name ? `Channel: ${channel?.name}` : ''}
+          </StyledSpan>
+        </Link>
+      )}
+    </StyledColLogoContainer>
+  )
+}
+
+export default CurrentChannel
